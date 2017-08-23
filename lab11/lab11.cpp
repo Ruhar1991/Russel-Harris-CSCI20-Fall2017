12452b91f7e5dc2fff107c1610a49b42c8fdc3a6
Russel Harris <ruhar1991@gmail.com>
2017-08-23 20:19:32 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,37 @@
+//start with 10 points
+the ability to guess does not exist
+A variable is set to 10
+
+//randomly select between 2 numbers
+A number is either 0 or 1
+Computer tells user to guess
+The ability to guess exists
+
+//user guesses
+if the ability to guess exists, the line below is in play
+wait until user guesses
+if user answers, the 3 lines below are in play
+store user's answer
+computer reply is in play
+user looses the ability to guess
+
+//if answer is in play
+if the computer reply is in play, all of the below is in play
+
+//if guessed right
+If the guess was right, computer tells user they're right and the game is over and the computer can no longer reply
+
+//if guessed wrong
+If the guess was wrong, the variable originally set to 10 is lowered by 1
+
+//higher or lower
+if player guessed under number, computer says to guess higher
+if player guessed over number, computer says to guess higher
+
+//guess again
+computer says to guess again
+the ability to guess exists again
+the computer reply is no longer in play
+
+//lose
+if the number originally set to 10 is 0, the player loses and the computer tells the user
